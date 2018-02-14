9b3ae4872e85866d9008512f153ebba96775bce1
Jakob Wierzba <github@jakobwierzba.de>
2018-02-14 12:57:13 +0100

    Keyboard navigation, better scene debug

--- dvdlikenav.js
+++ dvdlikenav.js
@@ -1,19 +1,23 @@
 (function (cfg, w, d, docEl, body) {
-	var I,
+	var I, Ic,
 		debug = {
 			"enabled": !!d.location.hash.match(/debug/),
 			"tsLog": true,
 			"highlightButtons": true,
 			"highlightUIBorder": true,
-			"videoControls": true
+			"videoControls": true,
+			"showKeyInput": true,
+			"showSceneSelector": true
 		},
 		v = d.getElementsByTagName('video'),
 		el = {},
 		uis = {},
 		currentScene = cfg.scenes.start,
 		idling = false,
+		keyInput = null,
+		isInInputWindow = false,
 		tsLog = function (str) {
-			el.ts.innerHTML = str;
+			el.ts.innerHTML = currentScene.id + ' ' + str;
 		},
 		checkProgress = function () {
 			if (idling) {
@@ -25,6 +29,18 @@
 			if (debug.enabled && debug.tsLog) {
 				tsLog(v[0].currentTime);
 			}
+			if (currentScene.keyWindow) {
+				isInInputWindow = currentScene.keyWindow.start <= v[0].currentTime && v[0].currentTime < currentScene.keyWindow.end;
+				if (debug.enabled && debug.showKeyInput) {
+					el.key.style.opacity = isInInputWindow ? 1.0 : 0.5;
+				}
+			}
+			else {
+				isInInputWindow = null;
+				if (debug.enabled && debug.showKeyInput) {
+					el.key.style.opacity = 0.1;
+				}
+			}
 			if (v[0].currentTime >= currentScene.end) {
 				console.log('scene ended');
 				if (currentScene.idleAtEnd === true) {
@@ -33,8 +49,23 @@
 					idling = true;
 					// idle ok
 				}
+				else if (typeof currentScene.after === 'string') {
+					console.log('going to after: ' + currentScene.after);
+					loadScene(currentScene.after);
+
+				}
+				else if (typeof currentScene.after === 'object') {
+					console.log('picking next scene by input: ', keyInput, currentScene.after);
+					if (currentScene.after.hasOwnProperty(keyInput)) {
+						loadScene(currentScene.after[keyInput]);
+					}
+					else {
+						console.log('no matching scene, defaulting'); 
+						loadScene(currentScene.after['default']);
+					}
+				}
 				else {
-					console.log('no buttons, going back to start');
+					console.log('no buttons, no after, going back to start');
 					loadScene('start');
 				}
 			}
@@ -61,10 +92,17 @@
 		loadScene = function (id) {
 			var ui;
 			console.log('loading scene', id);
+			keyInput = 'default';
+			if (debug.enabled && debug.showKeyInput) {
+				el.key.innerHTML = keyInput;
+			}
 			for (I in uis) { if (!uis.hasOwnProperty(I)) continue;
 				uis[I].style.display = 'none';
 			};
 			currentScene = cfg.scenes[id];
+			if (debug.enabled && debug.showSceneSelector) {
+				el.scenes.selectedIndex = currentScene.index;
+			}
 			idling = false;
 			v[0].currentTime = currentScene.start;
 			v[0].play();
@@ -90,11 +128,17 @@
 			}
 		}
 	}
-	['ts', 'uis', 'rfs'].forEach(function (id) {
+	['ts', 'uis', 'rfs', 'key', 'scenes'].forEach(function (id) {
 		el[id] = d.getElementById(id);
 	});
+	Ic = 0;
 	for (I in cfg.scenes) { if (!cfg.scenes.hasOwnProperty(I)) continue;
 		cfg.scenes[I].id = I;
+		cfg.scenes[I].index = Ic;
+		Ic++;
+		if (debug.enabled && debug.showSceneSelector) {
+			el.scenes.innerHTML += '<option value="' + I + '">' + I + '</option>';
+		}
 	}
 	console.log('init');
 	console.log('set movieFile', cfg.movieFile);
@@ -130,5 +174,31 @@
 		if (e.target.dataset.targetScene) {
 			loadScene(e.target.dataset.targetScene);
 		}
+	});
+	w.addEventListener('keydown', function (e) {
+		if (debug.enabled && debug.showKeyInput) {
+			console.log(e);
+		}
+		if (isInInputWindow) {
+			keyInput = e.key;
+			if (!currentScene.keyWindow.delayed) {
+				if (currentScene.after.hasOwnProperty(keyInput)) {
+					loadScene(currentScene.after[keyInput]);
+				}
+				else {
+					console.log('no matching scene, but not defaulting in immediate mode'); 
+					//loadScene(currentScene.after['default']);
+				}
+			}
+		}
+		else {
+			if (debug.enabled && debug.showKeyInput) {
+				console.log('key ' + e.key + ' ignored, not in window');
+			}
+		}
+		el.key.innerHTML = keyInput;
+	});
+	el.scenes.addEventListener('change', function (e) {
+		loadScene(el.scenes.selectedOptions[0].value);
 	});
 })(window.Dvdnavcfg, window, document, document.documentElement, document.getElementsByTagName('body')[0]);
